77356bbc1a7ad012b65ebb6196bbc8fc75b913d1
hackawax <bauwden.ramsey@gmail.com>
2017-08-30 14:05:47 +0200

    add fizzbuzz

---
+++ fizzbuzz/js/concat.js
@@ -0,0 +1,33 @@
+/*
+Exercice : FizzBuzz
+*/
+
+// Solution construisant le message à afficher par concaténation
+for (var nombre = 1; nombre <= 100; nombre++) {
+    var message = ""; // Message à afficher, initialement vide
+    if (nombre % 3 === 0) {
+        // Nombre divisible par 3 : on ajoute "Fizz" au message
+        message += 'Fizz';
+    }
+    if (nombre % 5 === 0) {
+        // Nombre divisible par 5 : on ajoute "Buzz" au message
+        message += 'Buzz';
+    }
+    if (message === "") {
+        // Si message est vide, le nombre n'est divisible ni par 3, ni par 5 :
+        // le message affiché sera le nombre
+        message = nombre;
+    }
+    console.log(message);
+}
+
+/*
+for (var n = 1; n <= 100; n++) {
+  var output = "";
+  if (n % 3 == 0)
+    output += "Fizz";
+  if (n % 5 == 0)
+    output += "Buzz";
+  console.log(output || n);
+}
+*/
